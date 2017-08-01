bee664760ffb5e38b113db3eb40186448962bda0
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2017-08-01 16:16:42 +0200

    Added a call to super in the Paladin class

--- ClassDesign.java
+++ ClassDesign.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 public class ClassDesign {
 
     private static final String LINEBREAK = "\n";
+    private static final String MARKER = "- ";
+    private static final String ATKOPTIONS = "Attack options:";
+    private static final String DEFOPTIONS = "Defense options:";
 
     public static void main(String[] args) {
         ClassDesign classDesign = new ClassDesign();
@@ -32,8 +35,12 @@ public class ClassDesign {
 
     private void displayCharacters(ArrayList<Character> charList) {
         for (Character c : charList) {
-            System.out.println(LINEBREAK + c.name);
+            System.out.println(LINEBREAK + MARKER + c.name);
+
+            System.out.println(LINEBREAK + ATKOPTIONS);
             c.attackOptions();
+
+            System.out.println(LINEBREAK + DEFOPTIONS);
             c.defenseOptions();
         }
     }
